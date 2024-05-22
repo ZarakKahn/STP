bac6f28274f7ae55c41fe045646e63ca2e0bf3d9
Zarak khan <73734224+ZarakKahn@users.noreply.github.com>
2024-05-22 12:04:19 +0500

    Update DashboardPage.jsx

--- DashboardPage.jsx
+++ DashboardPage.jsx
@@ -1,40 +1,27 @@
-<div className="flex flex-wrap items-center p-5 bg-gray-100">
-  <div className="w-full m-2">
-    <p className="mb-1">Date Select:</p>
-    <div className="border-2 border-gray-400 w-64">
-      <Datepicker
-        value={value}
-        onChange={handleValueChange}
-        primaryColor={"red"}
-        showShortcuts={true}
-        placeholder={"DD-MM-YY to DD-MM-YY"}
-      />
-    </div>
-  </div>
-  
-  <div className="m-2 w-60">
-    <p className="mb-1">Region:</p>
-    <Select
-      options={options}
-      className={dropStyle}
-    />
-  </div>
-
-  <div className="m-2 w-60">
-    <p className="mb-1">Handler:</p>
-    <Select
-      options={handlers}
-      className={dropStyle}
-    />
-  </div>
-
-  <div className="m-2 w-60">
-    <p className="mb-1">Team:</p>
-    <Select
-      options={teams}
-      className={dropStyle}
-    />
-  </div>
-
-  <button className="m-2 w-fit h-12 flex justify-center items-center rounded-md shadow-sm px-4 py-2 bg-red-700 text-white hover:bg-red-800">Search</button>
-</div>
+ <nav className={`lg:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
+                        <ul className="space-y-2 sm:space-y-0 sm:flex sm:space-x-3 object-right">
+                            <li><Link to="/dashboard" className="text-blue-900 
+                            text-xs font-sans">DASHBOARD</Link></li>
+                            <div className="group">
+                                <button className="text-blue-900
+                                text-xs font-sans">REPORTS</button>
+                                <div className="dropdown-menu absolute hidden group-hover:block bg-blue-900">
+                                    <ul className="list-reset">
+                                        <li><Link to="/topmarketsvisits" className="block px-4 py-2 text-sm text-gray-50 hover:bg-blue-500">Top Market Visitors/Area</Link></li>
+                                        <li><Link to="/mvdetailedreports" className="block px-4 py-2 text-sm text-gray-50 hover:bg-blue-500">MV Detailed Report</Link></li>
+                                        <li><Link to="/mvstatusreport" className="block px-4 py-2 text-sm text-gray-50 hover:bg-blue-500">MV Status Report</Link></li>
+                                        <li><Link to="/mvweeklyreport" className="block px-4 py-2 text-sm text-gray-50 hover:bg-blue-500">MVR APP Weekly Summary Report</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <li><Link to="/visitplanner" className="text-blue-900
+                            text-xs font-sans">VISIT PLANNER</Link></li>
+                            <li><Link to="locationview" className="text-blue-900
+                            text-xs font-sans">LOCATION VIEW</Link></li>
+                            <li><Link to="/hierarchy" className="text-blue-900
+                            text-xs font-sans">HIERARCHY MANAGEMENT</Link></li>
+                        </ul>
+                    </nav>
+                    <button className="lg:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                        <FiMenu />
+                    </button>
