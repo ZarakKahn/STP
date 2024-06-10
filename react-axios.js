6568459de126c26f2b565de5f48e2dbfe3ed59ec
Zarak khan <73734224+ZarakKahn@users.noreply.github.com>
2024-06-10 17:13:54 +0500

    Update react-axios.js

--- react-axios.js
+++ react-axios.js
@@ -1,38 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Select from 'react-select';
+<Select
+            options={[{ value: 'All', label: 'All' }, ...handlers]}
+            onChange={(value) => handleDropdownChange(value)}
+            className="w-full text-sm p-2 border-2 border-gray-400 rounded-md "
+          />
 
-const HandlerDropdown = () => {
-  const [handlers, setHandlers] = useState([]);
-
-  useEffect(() => {
-    const fetchHandlers = async () => {
-      try {
-        const response = await axios.get('http://portal.mashitec.com/SalesWebApi/api/GetHandler/?user=asim&region=ALL');
-        const handlerOptions = response.data.map(handler => ({ value: handler, label: handler }));
-        setHandlers(handlerOptions);
-      } catch (error) {
-        console.error('Error fetching handlers:', error);
-      }
-    };
-
-    fetchHandlers();
-  }, []);
-
-  const handleDropdownChange = (selectedOption) => {
-    console.log('Selected handler:', selectedOption);
-  };
-
-  return (
-    <div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
-      <p className="mb-1 text-center">Handler:</p>
-      <Select
-        options={[{ value: 'All', label: 'All' }, ...handlers]}
-        onChange={(value) => handleDropdownChange(value)}
-        className="w-full border-2 border-gray-400 rounded-md"
-      />
-    </div>
-  );
-};
-
-export default HandlerDropdown;
+              when i select a value from the select list there is an extra line with cursor which effect the seletc box
