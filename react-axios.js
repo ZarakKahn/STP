06b98580d53714a501911d76e5fc0287372c6dda
Zarak khan <73734224+ZarakKahn@users.noreply.github.com>
2024-06-10 16:31:50 +0500

    Update react-axios.js

--- react-axios.js
+++ react-axios.js
@@ -1,200 +1,22 @@
-import React, { useEffect, useState } from "react";
-import Datepicker from "react-tailwindcss-datepicker";
-import Select from "react-dropdown-select";
-import RegionChart from "../Components/RegionChart";
-import Cards from "../Components/Cards";
-import TopVisitors from "../Components/TopVisitors";
-import LeastVisitors from "../Components/LeastVisitors";
-import TopVisitedArea from "../Components/TopVisitedArea";
-import TopComplaints from "../Components/TopComplaints";
-import axios from "axios";
-
-const Dashboard = () => {
-  const [dateRange, setDateRange] = useState({
-    startDate: new Date(),
-    endDate: new Date().setMonth(11)
-  });
-
-  const [dropdownValues, setDropdownValues] = useState({
-    region: 'All',
-    handler: 'All',
-    team: 'All'
-  });
-
-  const [data, setData] = useState(null);
-  const [topVisitors, setTopVisitors] = useState(null);
-  const [leastVisitors, setLeastVisitors] = useState(null);
-  const [topComplaints, setTopComplaints] = useState(null);
-  const [topVisitedAreas, setTopVisitedArea] = useState(null);
-  const user = localStorage.getItem('email');
-  const [handlerOption , setHandlerOption] = useState([]);
-  const [selectedHandler, setSelectedHandler] = useState('All');
-
-  // API call for handlers
-  const getHandlers = async () => {
-    try {
-      const response = await axios.post(`http://portal.mashitec.com/SalesWebApi/api/GetHandler/?user=${user}&region=ALL`);
-      const options = response.data.map(handler => ({ value: handler, label: handler }));
-      setHandlerOption(options);
-    } catch (error) {
-      console.error("Error fetching handlers:", error);
-    }
-  };
-
-  useEffect(() => {
-    getHandlers();
-  }, []);
-
-  // Data calling for pie chart and cards
-  const getChartData = async () => {
-    const { startDate, endDate } = dateRange;
-    const { region, handler, team } = dropdownValues;
-    try {
-      const response = await axios.post(
-        `http://portal.mashitec.com/SalesWebApi/api/GetChartData/?from=${startDate}&to=${endDate}&region=${region}&subreg=${handler}&user=${user}&team=${team}`
-      );
-      console.log("Response from API:", response.data);
-      setData(response.data);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
-  // Data calling for top 10 visitors
-  const getTopVisitors = async () => {
-    const { startDate, endDate } = dateRange;
-    const { region, handler, team } = dropdownValues;
-    try {
-      const response = await axios.post(
-        `http://portal.mashitec.com/SalesWebApi/api/Gettopvisits/?from=${startDate}&to=${endDate}&region=${region}&subreg=${handler}&user=${user}&team=${team}`
-      );
-      console.log("Response from API:", response.data);
-      setTopVisitors(response.data);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
-  // Least visitors
-  const getLeastVisitors = async () => {
-    const { startDate, endDate } = dateRange;
-    const { region, handler, team } = dropdownValues;
-    try {
-      const response = await axios.post(
-        ` http://portal.mashitec.com/SalesWebApi/api/Getleastvisits/?from=${startDate}&to=${endDate}&region=${region}&subreg=${handler}&user=${user}&team=${team}`
-      );
-      console.log("Response from API:", response.data);
-      setLeastVisitors(response.data);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
-  // Top visited areas
-  const getTopAreas = async () => {
-    const { startDate, endDate } = dateRange;
-    const { region, handler, team } = dropdownValues;
-    try {
-      const response = await axios.post(
-        `http://portal.mashitec.com/SalesWebApi/api/Gettoparea/?from=${startDate}&to=${endDate}&region=${region}&handler=${handler}&user=${user}&team=${team}`
-      );
-      console.log("Response from API:", response.data);
-      setTopVisitedArea(response.data);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
-  // Top complaints
-  const getTopComplaints = async () => {
-    const { startDate, endDate } = dateRange;
-    const { region, handler, team } = dropdownValues;
-    try {
-      const response = await axios.post(
-        `http://portal.mashitec.com/SalesWebApi/api/GetComplaints/?from=${startDate}&to=${endDate}&region=${region}&handler=${handler}&user=${user}&team=${team}`
-      );
-      console.log("Response from API:", response.data);
-      setTopComplaints(response.data);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
-  const handleValueChange = newValue => {
-    console.log("newValue:", newValue);
-    setDateRange(newValue);
-  };
-
-  const handleDropdownChange = (selectedOption, field) => {
-    setDropdownValues(prevState => ({
-      ...prevState,
-      [field]: selectedOption[0].value
-    }));
-  };
-
-  const fetchData = async () => {
-    await getChartData();
-    await getTopVisitors();
-    await getLeastVisitors();
-    await getTopAreas();
-    await getTopComplaints();
-  };
-
-  return (
-    <>
-      <div className="text-center h-16 bg-blue-900 border-b-4 border-red-800">
-        <h1 className="font-thin text-lg text-gray-50 py-5 tracking-widest">SALES VISITS DASHBOARD</h1>
-      </div>
-
-      <div className="h-auto flex flex-wrap items-center justify-center p-5 bg-gray-100 w-auto">
-        <div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
-          <p className="mb-1 text-center">Date Select:</p>
-          <div className="border-2 border-gray-400 w-full">
-            <Datepicker
-              value={dateRange}
-              onChange={handleValueChange}
-              primaryColor={"red"}
-              showShortcuts={true}
-              placeholder={"DD-MM-YY to DD-MM-YY"}
-            />
-          </div>
-        </div>
-
-        <div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
-          <p className="mb-1 text-center">Region:</p>
-          <Select
-            options={[
-              { value: 'All', label: 'All' },
-              { value: 'Baluchistan', label: 'Baluchistan' },
-              { value: 'Central', label: 'Central' },
-              { value: 'North', label: 'North' },
-              { value: 'South', label: 'South' }
-            ]}
-            onChange={(value) => handleDropdownChange(value, 'region')}
-            className="w-full text-sm border-2 border-gray-400 rounded-md"
-          />
-        </div>
-
-        <div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
+http://portal.mashitec.com/SalesWebApi/api/GetHandler/?user=asim&region=ALL
+call the above api in react using axios and populate the below select dropdown with the result of api 
+  the api result is as ["AL GHANI LUBRICANTS-HANDLER","AL HASSAN TRADERS","AZEEM AGENCIES","BUSINESS INTELLIGENCE HANDLER (NEW)","DYNAMIC ENTERPRISES","HORIZON ASSOCIATES","TOPLINK ENTERPRISES","TRADE N MOVE (PRIVATE) LIMITED"]
+ <div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
           <p className="mb-1 text-center">Handler:</p>
           <Select
-            options={handlerOption}
-            onChange={(value) => handleDropdownChange(value, 'handler')}
-            value={handlerOption.find(option => option.value === selectedHandler)}
-            className="w-full h-auto text-sm border-2 border-gray-400 rounded-md"
-          />
-        </div>
-
-        <div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
-          <p className="mb-1 text-center">Team:</p>
-          <Select
             options={[
               { value: 'All', label: 'All' },
-              { value: 'ASM', label: 'ASM' },
-              { value: 'TSO', label: 'TSO' },
+              { value: 'AL GHANI LUBRICANTS-HANDLER', label: 'AL GHANI LUBRICANTS-HANDLER' },
+              { value: 'AL HASSAN TRADERS', label: 'AL HASSAN TRADERS' },
+              { value: 'TRADE N MOVE (PRIVATE) LIMITED', label: 'TRADE N MOVE (PRIVATE) LIMITED' },
+              { value: 'TOPLINK ENTERPRISES', label: 'TOPLINK ENTERPRISES' },
+              { value: 'DYNAMIC ENTERPRISES', label: 'DYNAMIC ENTERPRISES' },
+              { value: 'UMID OIL AGENCY', label: 'UMID OIL AGENCY' },
+              { value: 'KHATTAK ENTERPRISES', label: 'KHATTAK ENTERPRISES' },
+              { value: 'SN ENTERPRISES', label: 'SN ENTERPRISES' },
+              { value: 'KHAIRPUR ENTERPRISES HANDLER (INTERIOR SINDH)', label: 'KHAIRPUR ENTERPRISES HANDLER (INTERIOR SINDH)' },
             ]}
-            onChange={(value) => handleDropdownChange(value, 'team')}
-            className="w-full text-sm border-2 border-gray-400 rounded-md"
+            onChange={(value) => handleDropdownChange(value, 'handler')}
+            className="w-full border-2 border-gray-400 rounded-md"
           />
         </div>
-        <div className="flex
