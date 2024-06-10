67801ae2e83231f0017e6d2c4460e1472d6fa786
Zarak khan <73734224+ZarakKahn@users.noreply.github.com>
2024-06-10 10:37:28 +0500

    react-axios.js

--- react-axios.js
+++ react-axios.js
@@ -1,66 +1,19 @@
-import React, { useState } from 'react';
-import axios from 'axios'; // Import Axios library
-
-function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    // Construct the URL with concatenated login and passw parameters
-    const apiUrl = 'http://portal.mashitec.com/SalesWebApi/api/User1/';
-    const requestBody = {
-      login: username,
-      passw: password
-    };
-
-    try {
-      // Make POST request to API using Axios
-      const response = await axios.post(apiUrl, requestBody);
-
-      // Check if the response matches the username
-      if (response.data.toLowerCase() === username.toLowerCase()) {
-        setMessage('Authorized');
-      } else {
-        setMessage('Not Authorized');
-      }
-    } catch (error) {
-      setMessage('Error: ' + error.message);
-    }
-  };
-
-  return (
-    <div>
-      <h2>Login Form</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+<div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
+          <p className="mb-1 text-center">Handler:</p>
+          <Select
+            options={[
+              { value: 'All', label: 'All' },
+              { value: 'AL GHANI LUBRICANTS-HANDLER', label: 'AL GHANI LUBRICANTS-HANDLER' },
+              { value: 'AL HASSAN TRADERS', label: 'AL HASSAN TRADERS' },
+              { value: 'TRADE N MOVE (PRIVATE) LIMITED', label: 'TRADE N MOVE (PRIVATE) LIMITED' },
+              { value: 'TOPLINK ENTERPRISES', label: 'TOPLINK ENTERPRISES' },
+              { value: 'DYNAMIC ENTERPRISES', label: 'DYNAMIC ENTERPRISES' },
+              { value: 'UMID OIL AGENCY', label: 'UMID OIL AGENCY' },
+              { value: 'KHATTAK ENTERPRISES', label: 'KHATTAK ENTERPRISES' },
+              { value: 'SN ENTERPRISES', label: 'SN ENTERPRISES' },
+              { value: 'KHAIRPUR ENTERPRISES HANDLER (INTERIOR SINDH)', label: 'KHAIRPUR ENTERPRISES HANDLER (INTERIOR SINDH)' },
+            ]}
+            onChange={(value) => handleDropdownChange(value, 'handler')}
+            className="w-full border-2 border-gray-400 rounded-md"
           />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <p id="message">{message}</p>
-    </div>
-  );
-}
-
-export default LoginForm;
-
