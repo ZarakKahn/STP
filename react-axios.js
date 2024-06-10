4f59d99c16c891ef094eabfc0015bbb75477c079
Zarak khan <73734224+ZarakKahn@users.noreply.github.com>
2024-06-10 16:07:08 +0500

    Update react-axios.js

--- react-axios.js
+++ react-axios.js
@@ -1,19 +1,34 @@
-<div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
+ const [handlerOption , setHandlerOption] = useState([]);
+  const [selectedHandler, setSelectedHandler] = useState('All');
+const getHandlers = async () => {
+    try {
+      const response = await axios.post(`http://portal.mashitec.com/SalesWebApi/api/GetHandler/?user=${user}&region=ALL`);
+      setHandlerOption(response.data);
+    } catch (error) {
+      console.error("Error fetching handlers:",error);
+    };
+  };
+  useEffect(() => {
+    getHandlers();},[])
+
+ const handleDropdownChange = (value, field) => {
+    setDropdownValues(prevState => ({
+      ...prevState,
+      [field]: value[0].value
+    }));
+    setSelectedHandler(value.value);
+  };
+
+
+
+how can i put the result of the above function in the below select component
+
+ <div className="w-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-2">
           <p className="mb-1 text-center">Handler:</p>
           <Select
-            options={[
-              { value: 'All', label: 'All' },
-              { value: 'AL GHANI LUBRICANTS-HANDLER', label: 'AL GHANI LUBRICANTS-HANDLER' },
-              { value: 'AL HASSAN TRADERS', label: 'AL HASSAN TRADERS' },
-              { value: 'TRADE N MOVE (PRIVATE) LIMITED', label: 'TRADE N MOVE (PRIVATE) LIMITED' },
-              { value: 'TOPLINK ENTERPRISES', label: 'TOPLINK ENTERPRISES' },
-              { value: 'DYNAMIC ENTERPRISES', label: 'DYNAMIC ENTERPRISES' },
-              { value: 'UMID OIL AGENCY', label: 'UMID OIL AGENCY' },
-              { value: 'KHATTAK ENTERPRISES', label: 'KHATTAK ENTERPRISES' },
-              { value: 'SN ENTERPRISES', label: 'SN ENTERPRISES' },
-              { value: 'KHAIRPUR ENTERPRISES HANDLER (INTERIOR SINDH)', label: 'KHAIRPUR ENTERPRISES HANDLER (INTERIOR SINDH)' },
-            ]}
-            onChange={(value) => handleDropdownChange(value, 'handler')}
-            className="w-full border-2 border-gray-400 rounded-md"
+          options={handlerOption}
+            onChange={handleDropdownChange}
+              values={[{ value: selectedHandler }]}
+            className="w-full h-auto text-sm border-2 border-gray-400 rounded-md"
           />
         </div>
